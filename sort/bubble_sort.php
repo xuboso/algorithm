09654e08a540f9d893ca062cc2890fa6bc35e997
bude <xubobox@gmail.com>
2016-07-21 17:07:27 +0800

    add bubble sort

---
+++ sort/bubble_sort.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * bubble sort
+ */
+$a = [12, 1, 8, 9, 23, 36, 48, 52, 66, 72, 80, 91];
+$len = count($a);
+
+for ($i = 0; $i < $len; $i++) {
+    for ($j = $len - 1; $j > $i; $j--) {
+        // swap 
+        if ($a[$i] > $a[$j]) {
+            $temp = $a[$j];
+            $a[$j] = $a[$i];
+            $a[$i] = $temp;
+        }
+    }
+}
+
+var_dump($a);
