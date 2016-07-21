196ad85f509d40c9a36b39655054db5ef7669965
bude <xubobox@gmail.com>
2016-07-21 17:15:10 +0800

    update bubble sort

--- sort/bubble_sort.php
+++ sort/bubble_sort.php
@@ -7,7 +7,7 @@ $a = [12, 1, 8, 9, 23, 36, 48, 52, 66, 72, 80, 91];
 $len = count($a);
 
 for ($i = 0; $i < $len; $i++) {
-    for ($j = $len - 1; $j > $i; $j--) {
+    for ($j = $len - 1 - $i; $j > $i; $j--) {
         // swap 
         if ($a[$i] > $a[$j]) {
             $temp = $a[$j];
